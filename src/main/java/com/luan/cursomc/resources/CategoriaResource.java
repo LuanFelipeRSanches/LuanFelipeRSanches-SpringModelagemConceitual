955c5345690b7824ce0e38083c06e923cde790d3
LuanFelipeRSanches <unopararchspire@hotmail.com>
2019-01-30 17:41:52 -0300

    Projeto criado

---
+++ src/main/java/com/luan/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,11 @@
+package com.luan.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+private Integer id;
+private 
+}
